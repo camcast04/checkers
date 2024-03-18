91dbd37d5760bdb8afa960e95cb8dc9bad2dd4fc
camcast04 <cam.cast04@gmail.com>
2024-03-18 18:05:40 -0400

    added more necessary cached elements

--- script.js
+++ script.js
@@ -18,7 +18,18 @@ let possibleMoves = [];
 /*----- cached elements  -----*/
 const lightPieces = document.querySelectorAll('.light');
 const darkPieces = document.querySelectorAll('.dark');
+const blackSquares = document.querySelectorAll('.black');
+const gameBoard = document.getElementById('gameboard');
+const playerDisplay = document.getElementById('player');
+const infoDisplay = document.getElementById('info-display');
+const resetButton = document.getElementById('reset');
+const squares = document.querySelectorAll('.square');
 /*----- event listeners -----*/
 
 /*----- functions -----*/
-function init() {}
+function init() {
+  renderBoard();
+  renderPieces();
+}
+function renderBoard() {}
+function renderPieces() {}
