75e873d62b197442fa3160ceb26fdb721eb586dd
camcast04 <cam.cast04@gmail.com>
2024-03-18 14:43:00 -0400

    adding js functions

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 /*----- constants -----*/
 
 /*----- state variables -----*/
+const board = [
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 0, 2, 0, 2, 0, 2, 0],
+  [0, 2, 0, 2, 0, 2, 0, 2],
+  [2, 0, 2, 0, 2, 0, 2, 0],
+];
+
+let currentPlayer = 1;
+let possibleMoves = [];
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+function init() {}
