ca14e2ad9828fe07f703d0d2ac370c992080ad72
camcast04 <cam.cast04@gmail.com>
2024-03-18 18:13:53 -0400

    created player diplay function and event listener for reset button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /*----- constants -----*/
-
+const PLAYER_COLORS = {
+  player1: '#f4f7fa',
+  player2: '#34354e',
+};
 /*----- state variables -----*/
 const board = [
   [0, 1, 0, 1, 0, 1, 0, 1],
@@ -25,11 +28,23 @@ const infoDisplay = document.getElementById('info-display');
 const resetButton = document.getElementById('reset');
 const squares = document.querySelectorAll('.square');
 /*----- event listeners -----*/
-
+resetButton.addEventListener('click', init);
 /*----- functions -----*/
+init();
+
 function init() {
   renderBoard();
   renderPieces();
+  currentPlayer = 1;
+  updatePlayerDisplay();
 }
+
 function renderBoard() {}
-function renderPieces() {}
+function renderPieces() {
+  lightPieces.forEach((piece) => piece.remove);
+  darkPieces.forEach((piece) => piece.remove);
+}
+
+function updatePlayerDisplay() {
+  playerDisplay.textContent = currentPlayer === 1 ? 'Player 1' : 'Player 2';
+}
